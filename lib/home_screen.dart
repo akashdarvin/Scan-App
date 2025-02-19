16c4ac8357f516b6051ff698f6b798e170e70fae
akashdarvin <akashdarvin47@gmail.com>
2025-02-19 20:04:13 +0530

    CHANGED SOMETHING

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,98 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'mydrawer.dart';
-// import 'scanidtile.dart';
-
-// class HomeScreen extends StatelessWidget {
-//  HomeScreen({super.key});
-// final ImagePicker _picker = ImagePicker();
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         drawer: MyDrawer(),
-//         appBar: AppBar(
-          
-//           leading: Builder(builder: (context) {
-//             return InkWell(
-//               onTap: () {
-//                 Scaffold.of(context).openDrawer();
-//               },
-//               child: Icon(
-//                 Icons.menu,
-//                 color: Colors.white,
-//               ),
-//             );
-//           }),
-//           backgroundColor: const Color.fromARGB(255, 0, 9, 33),
-//           title: Text(
-//             "ScanMe",
-//             style: TextStyle(
-//               color: Colors.white,
-//               fontSize: 30,
-//               fontWeight: FontWeight.bold,
-//               fontStyle: FontStyle.italic,
-//             ),
-//           ),
-//         ),
-//         backgroundColor: const Color.fromARGB(255, 26, 1, 69),
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               // 1st button for front id
-//               ScanIdTile(title: "Scan Front of ID Card",
-//               onPressed: (){
-//                 _openCamera();
-//               },),
-//               SizedBox(
-//                 height: 80,
-//               ),
-//               //2nd button
-//               ScanIdTile(title: "Scan Back of ID Card",
-//               onPressed: (){
-//                 _openCamera();
-//                 },),
-//               SizedBox(
-//                 height: 100,
-//               ),
-//               // upload image from gallery button
-//               ElevatedButton.icon(
-//                   onPressed: () {
-//                     //gallery picker
-//                   },
-//                   style: ElevatedButton.styleFrom(
-//                       backgroundColor: Colors.white,
-//                       shape: RoundedRectangleBorder(
-//                           borderRadius: BorderRadius.circular(30)),
-//                       minimumSize: Size(100, 50)),
-//                   icon: Icon(
-//                     Icons.upload,
-//                     size: 20,
-//                     color: Colors.black,
-//                   ),
-//                   label: Text(
-//                     "Upload Image",
-//                     style: TextStyle(
-//                         fontSize: 20,
-//                         fontWeight: FontWeight.bold,
-//                         color: Colors.black),
-//                   ))
-//             ],
-//           ),
-//         ),
-//       );
-//   }
-//    Future<void> _openCamera() async {
-//     final XFile? image = await _picker.pickImage(source: ImageSource.camera,);
-
-//     if (image != null) {
-//       print("Image Path: ${image.path}");
-//     }
-//   }
-// }
-
-
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'scanidtile.dart';
@@ -121,15 +26,11 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {},
           ),
           IconButton(
-            icon: Icon(Icons.history, color: Colors.white),
+            icon: Icon(Icons.person, color: Colors.white),
             onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.settings, color: Colors.white),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.person, color: Colors.white),
             onPressed: () {},
           ),
         ],
